9027011a296942a6c75c838dc4c1a587cbcce53d
Jan <jankretschmann1@gmail.com>
2021-11-13 12:54:11 -0600

    contingency table saved in class correctly with incr and decrement

--- js/application.js
+++ js/application.js
@@ -1,17 +1,32 @@
+import YoungTableu from "./tableau";
+
 export default class Application {
   constructor() {
+    this.contingencyTable = [
+      [0, 0, 0],
+      [0, 0, 0],
+      [0, 0, 0],
+    ];
     this.initializeEventListeners();
   }
 
-  incrementCell(cell) {
+  computeTableau() {}
+
+  incrementCell(cell, index) {
     let val = parseInt(cell.innerHTML);
     cell.innerHTML = val + 1;
+    let i = index[0];
+    let j = index[1];
+    ++this.contingencyTable[i][j];
   }
 
-  decrementCell(cell) {
+  decrementCell(cell, index) {
     let val = parseInt(cell.innerHTML);
     if (val > 0) {
       cell.innerHTML = val - 1;
+      let i = index[0];
+      let j = index[1];
+      --this.contingencyTable[i][j];
     } else {
       console.warn("Only positive integers allowed!");
     }
@@ -20,10 +35,18 @@ export default class Application {
   initializeEventListeners() {
     let items = document.getElementsByTagName("td");
     for (let i = 0; i < items.length; ++i) {
-      items[i].addEventListener("click", () => this.incrementCell(items[i]));
+      // compute row and column number from i
+      let cols = this.contingencyTable[0].length;
+
+      let row = Math.floor(i / cols);
+      let col = i % cols;
+      let index = [row, col];
+      items[i].addEventListener("click", () => {
+        this.incrementCell(items[i], index);
+      });
       items[i].addEventListener("contextmenu", (event) => {
         event.preventDefault();
-        this.decrementCell(items[i]);
+        this.decrementCell(items[i], index);
       });
     }
   }
